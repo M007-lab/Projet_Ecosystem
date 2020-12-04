1f1c347612ded7d9ffea2af11b4c615e97476958
SoribaD <soriba.dg@gmail.com>
2020-12-04 13:06:53 +0100

    create header for Kamikaze

--- Kamikaze.h
+++ Kamikaze.h
@@ -1,5 +1,13 @@
-#include "Comportement.h"
-class Kamikaze:public Comportement
-{
+#ifndef KAMIKAZE
+#define KAMIKAZE
 
-}
+#include "Comportement.h"
+class Kamikaze:public Comportement{
+public:
+    void executeBehavior(Bestiole* bestiole) override;
+    Comportement* dupliquer() const override;
+     ~Kamikaze(){};
+    std::string getName() override;
+};
+
+#endif
