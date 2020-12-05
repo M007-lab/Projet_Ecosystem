2cd9ae1ba61e3d267f74510a5bbef52177f570d1
SoribaD <soriba.dg@gmail.com>
2020-12-05 03:22:56 +0100

    replace directMoyenne by directionMoyenne

--- Peureuse.cpp
+++ Peureuse.cpp
@@ -4,13 +4,13 @@
 void Peureuse::executeBehavior(Bestiole* bestiole) override{
     std::list<Bestiole*> voisins = bestiole->getVoisins();
     if(voisins.size()>((Config::getInstance()->seuilPeureuse)){
-            directMoyenne = 0;
+            directionMoyenne = 0;
             for (auto b : voisins) {
-                directMoyenne += b->getDirection();
+                directionMoyenne += b->getDirection();
             }
-            directMoyenne /= voisins.size();
+            directionMoyenne /= voisins.size();
 
-            bestiole->setDirection(-directMoyenne)
+            bestiole->setDirection(-directionMoyenne)
     }
 }
 
